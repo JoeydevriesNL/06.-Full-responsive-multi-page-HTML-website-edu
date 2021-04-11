24b9dbe37c290f9c909a0822c4dea2b5ccc1fdd5
JoeydevriesNL <46714256+JoeydevriesNL@users.noreply.github.com>
2021-04-11 19:20:42 +0200

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@joeydevries.nl';
+$email_from = '46714256+JoeydevriesNL@users.noreply.github.com';
 
 $email_subject = 'New Form Submission';
 
